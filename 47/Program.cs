49f686cbb65123734e808e57ddb64a16ec9e3b08
Tatyana <tatyana.volkova1984@mail.ru>
2022-12-03 01:01:36 +0500

    Задаkf двумерный массив размером m×n, заполненный случайными вещественными числами. m = 3, n = 4.

---
+++ 47/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9 */
+int rows = ReadInt("Введите количество строк m: ");
+int columns = ReadInt("Введите количество столбцов n: ");
+double[,] numbers = new double[rows, columns];
+FillMatrixRandomNumbers(numbers);
+WriteMatrix(numbers);
+
+void FillMatrixRandomNumbers(double[,] array)
+{
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+array[i, j] = new Random().Next(-10, 10);
+}
+}
+}
+
+void WriteMatrix(double[,] array)
+{
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+Console.Write(array[i, j] + " ");
+}
+Console.WriteLine();
+}
+Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
