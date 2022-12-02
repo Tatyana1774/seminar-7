b494c79b39e96ca7118948e70808e478ebf3f63b
Tatyana <tatyana.volkova1984@mail.ru>
2022-12-03 01:08:50 +0500

    Сделала только положительные числа

--- 52/Program.cs
+++ 52/Program.cs
@@ -28,7 +28,7 @@ for (int i = 0; i < array.GetLength(0); i++)
 {
 for (int j = 0; j < array.GetLength(1); j++)
 {
-array[i, j] = new Random().Next(-10, 10);
+array[i, j] = new Random().Next(1, 10);
 }
 }
 }
